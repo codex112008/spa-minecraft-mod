af7cc6c373995fcceb1340b0545d9e21103598ad
Codex112008 <codexofcode1120@gmail.com>
2026-05-26 09:41:07 +0800

    init

---
+++ src/main/java/net/penguincodex/spamod/SPA.java
@@ -0,0 +1,16 @@
+package net.penguincodex.spamod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SPA implements ModInitializer {
+	public static final String MOD_ID = "spa";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/penguincodex/spamod/SPAClient.java
@@ -0,0 +1,10 @@
+package net.penguincodex.spamod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SPAClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
